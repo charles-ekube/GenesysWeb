7e3f9fff0db829fc67ab2715865ae4354ec4f482
Charles Ekube <60458135+charles-ekube@users.noreply.github.com>
2020-10-17 22:25:46 +0100

    added react-router to link the components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Navigation from './Navigations';
 import LandingPage from './LandingPage';
-// import Footer from './Footer';
 import FaqPage from './FaqPage';
-
 import SoftwareDevelopersPage from './SoftwareDevelopersPage';
 import ProductDesignPage from './ProductDesignPage';
 import ApplyPage from './ApplyPage';
 import JournalPage from './JournalPage';
 import LearnableHomePage from './LearnableHomePage';
+import Footer from './Footer';
+import './Pages.css';
+
 
 class App extends React.Component {
   render() {
     return (
       <>
-        
-        {/*  */}
-        {/* */}
-        {/*  */}
-        {/* */}
-        {/* / */}
-        {/* */}
-        
-        {/* <Footer/> */} 
         <Router>
             <Navigation/>
-            {/* <LandingPage/> */}
-            <LearnableHomePage />
-            {/* <SoftwareDevelopersPage />  */}
-            {/* <ProductDesignPage /> */}
-            {/* <ApplyPage /> */}
-            {/* <JournalPage /> */}
-            {/* <FaqPage /> */}
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                    <LandingPage/>
+              </React.Fragment>
+            )}/>
+            <Route path="/LearnableHomePage" component={LearnableHomePage}/>
+            <Route path="/SoftwareDevelopersPage" component={SoftwareDevelopersPage}/>
+            <Route path="/ProductDesignPage" component={ProductDesignPage}/>
+            <Route path="/ApplyPage" component={ApplyPage}/>
+            <Route path="/JournalPage" component={JournalPage}/>
+            <Route path="/FaqPage" component={FaqPage}/>
+            <Footer/>
         </Router>
       </>
         
