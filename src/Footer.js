0588dc3b43d17a6fd274da1edd03269ae15f30a5
Charles Ekube <60458135+charles-ekube@users.noreply.github.com>
2020-10-18 15:19:02 +0100

    final update

--- src/Footer.js
+++ src/Footer.js
@@ -9,22 +9,22 @@ class Footer extends React.Component {
                 <section className="quick-links">
                     <ul>
                         <li>
-                            <Link to="/SoftwareDevelopersPage">
-                                LEARN MORE 
+                            <Link to="/SoftwareDevelopersPage" className="navlink">
+                                BUILD SOFTWARE
                             </Link>    
                         </li>
                         <li>
-                            <Link to="/ProductDesignPage">
+                            <Link to="/ProductDesignPage" className="navlink">
                                 DESIGN DIFFERENT 
                             </Link>    
                         </li>
                         <li>
-                            <Link to="/ApplyPage">
+                            <Link to="/ApplyPage" className="navlink">
                                 APPLY YOUR KNOWLEDGE 
                             </Link>    
                         </li>
                         <li>
-                            <Link to="/FaqPage">
+                            <Link to="/FaqPage" className="navlink">
                                 FAQ 
                             </Link>    
                         </li>
@@ -36,46 +36,46 @@ class Footer extends React.Component {
                         Expressway, Centenary City, Enugu. Nigeria
                     </address>
                     <span>
-                        <a href="#">GET IN TOUCH</a>
+                        <p>GET IN TOUCH</p>
                     </span>
                 </section>
                 <section className="devstudio-footer-link">
-                    <img src={require("./stand-project/footer-img.png")} alt="Footer-Image"/>
+                    <img src={require("./stand-project/footer-img.png")} alt="FooterImage"/>
                     <div>
                         <p>
                             Learnable is proud to be a collaborative
                             ffort of all the arms of our product team
                         </p>
-                        <a href="#">HEAD TO DEVSTUDIO</a>
+                        <p>HEAD TO DEVSTUDIO</p>
                     </div>
                 </section>
             
                 <section className="social-section">
                     <div className="social-media-links">
                         <ul>
-                            <li><a href="#">
-                                    <i className="fa fa-twitter fa-2x"></i>
-                                </a></li>
-                            <li><a href="#">
-                                    <i className="fa fa-facebook fa-2x"></i>
-                                </a></li>
-                            <li><a href="#">
-                                    <i className="fa fa-linkedin fa-2x"></i>
-                                </a></li>
-                            <li><a href="#">
-                                    <i className="fa fa-instagram fa-2x"></i>
-                                </a></li>
-                            <li><a href="#">
-                                    <i className="fa fa-youtube fa-2x"></i>
-                                </a></li>
-                            <li><a href="#">
-                                    <i className="fa fa-vimeo fa-2x"></i>
-                                </a></li>
+                            <li>
+                                <i className="fa fa-twitter fa-2x"></i>
+                            </li>
+                            <li>
+                                <i className="fa fa-facebook fa-2x"></i>
+                            </li>
+                            <li>
+                                <i className="fa fa-linkedin fa-2x"></i>
+                            </li>
+                            <li>
+                                <i className="fa fa-instagram fa-2x"></i>
+                            </li>
+                            <li>
+                                <i className="fa fa-youtube fa-2x"></i>
+                            </li>
+                            <li>
+                                <i className="fa fa-vimeo fa-2x"></i>
+                            </li>
                         </ul>
                     </div>
                     <div className="copyright-link">
                         <span><i className="fa fa-copyright "></i>GENESYS 2020</span>
-                        <span><a href="#">PRIVACY STATEMENT</a></span>
+                        <span><p>PRIVACY STATEMENT</p></span>
                     </div>
                 </section>
             </footer>
