1d17f5c9fb58efabb2ed7b0050110bffb8e5e1ee
Charles Ekube <60458135+charles-ekube@users.noreply.github.com>
2020-10-18 02:00:45 +0100

    Updated the Navigation component

--- src/Navigations.js
+++ src/Navigations.js
@@ -1,38 +1,37 @@
 import React, {useState} from 'react';
 import './App.css';
-// import Footer from './Footer';
-// import Faq from './FAQ';
-// import LearnableNav from './LearnableNav';
-// import SoftwareDevelopers from './SoftwareDevelopers';
-// import ProductDesign from './ProductDesign';
-// import Apply from './Apply';
-// import Journal from './Journal';
+import {Link} from 'react-router-dom';
 
 function Navigation() {
 
   const [showMenu, setShowMenu] = useState(false);
   let menu 
   if(showMenu) {
-    menu =  <ul className="mobile-nav-links">
-    <li>
-      <button onClick={() => setShowMenu(false)}>*</button>
-    </li>
-    <li><a href="#">LEARNABLE</a></li>
-    <li><a href="#">AGORA</a></li>
-    <li><a href="#">STARTZONE</a></li>
-    <li><a href="#">ABOUT</a></li>
-    <li><a href="#">CONTACT</a></li>
-    <li><a href="#">DEVSTUDIO</a></li>
-  </ul>
+    menu = 
+        <div className="mobile-nav-links">
+           <ul>
+              <li>
+                <button onClick={() => setShowMenu(false)}><i className="fa fa-times fa-2x"></i></button>
+              </li>
+              <li><Link to="/LearnableHomePage" className="navlink">LEARNABLE</Link></li>
+              <li><a href="#">AGORA</a></li>
+              <li><a href="#">STARTZONE</a></li>
+              <li><a href="#">ABOUT</a></li>
+              <li><a href="#">CONTACT</a></li>
+              <li><a href="#">DEVSTUDIO</a></li>
+          </ul>
+        </div>
   }
     return (
         <nav className="main-nav">
-             <a href="#" className="logo">
-               <img src={require("./stand-project/genesyslogo.png")} alt="logo"/> 
-            </a> 
+          
+          <Link to="/">
+               <img src={require("./stand-project/genesyslogo.png")} alt="logo" className="logo"/> 
+           </Link>
+              
             <div className="nav-links">
               <ul>
-                <li><a>LEARNABLE</a></li>
+                <li><Link to="/LearnableHomePage" className="navlink">LEARNABLE</Link></li>
                 <li><a>AGORA</a></li>
                 <li><a>STARTZONE</a></li>
                 <li><a>ABOUT</a></li>
@@ -44,10 +43,12 @@ function Navigation() {
               <button>DEVSTUDIO</button>
             </div>
             <div className="mobile-nav-btn">
-            <button onClick={() => setShowMenu(true)} >=</button>
+            <button onClick={() => setShowMenu(true)} ><i className="fa fa-bars fa-2x"></i></button>
             </div>
-              
             {menu}
+           
+          
+         
         </nav>
     )
 } 
