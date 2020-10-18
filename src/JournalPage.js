9a7ad7fb6301788a14595886678e4d1a3de7dbd8
Charles Ekube <60458135+charles-ekube@users.noreply.github.com>
2020-10-18 03:26:01 +0100

    completed the journal page component

---
+++ src/JournalPage.js
@@ -0,0 +1,160 @@
+import React from 'react';
+import './Pages.css';
+import './media.css';
+import LearnableNav from './LearnableNav';
+
+class Journal extends React.Component {
+    render() {
+        return (
+            <main>
+                <LearnableNav/>
+                <section className="journal-intro">
+                    <div className='journal-head'>
+                        <h1>Journal</h1>
+                        <button>CATEGORIES</button>
+                    </div>
+                    <article className="journal-inspiration">
+                        <img src={require("./stand-project/applyimg.png")} alt="journal"/>
+                        <div>
+                            <a href="#">INSPIRATION</a>
+                            <h2>
+                                Learn Design Thinking
+                                like a Pro and Build
+                                Projects that People
+                                will care about
+                            </h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit
+                                sapien arcu vitae orci at molestie. Pharetra quisque donec
+                                accumsan rhoncus risus. Urna magna turpis duis imperdiet
+                                elit, et, hendrerit viverra risus. Vulputate euismod commodo
+                                donec tincidunt et, aliquam. Eget scelerisque netus habitant
+                                dui porttitor cursus. Ipsum sem ut tortor ac ullamcorper.
+                                Lacus.
+                            </p>
+                            <div className="journal-author">
+                                <img src={require("./stand-project/journal-intro.png")} alt="journal"/>
+                                <span>David Mong, Apr 9, 2020</span>
+                            </div>
+                        </div>
+                    </article>
+                </section>
+                <section className="journal-cards">
+                    <div className="journal-cards-container">
+                        <article>
+                            <img src={require("./stand-project/journal-intro.png")} alt="journal" />
+                            <a href="#">INSPIRATION</a>
+                            <h2>
+                                Learn Design Thinking
+                                like a Pro and Build
+                                Projects that People
+                                will care about
+                            </h2>
+                            
+                            <span>David Mong, Apr 9, 2020</span>
+                        </article>
+                        <article>
+                            <img src={require("./stand-project/journal-intro.png")} alt="journal" />
+                            <a href="#">INSPIRATION</a>
+                            <h2>
+                                Learn Design Thinking
+                                like a Pro and Build
+                                Projects that People
+                                will care about
+                            </h2>
+                            
+                            <span>David Mong, Apr 9, 2020</span>
+                        </article>
+                        <article>
+                            <img src={require("./stand-project/journal-intro.png")} alt="journal" />
+                            <a href="#">INSPIRATION</a>
+                            <h2>
+                                Learn Design Thinking
+                                like a Pro and Build
+                                Projects that People
+                                will care about
+                            </h2>
+                            
+                            <span>David Mong, Apr 9, 2020</span>
+                        </article>
+                        <article>
+                            <img src={require("./stand-project/journal-intro.png")} alt="journal" />
+                            <a href="#">INSPIRATION</a>
+                            <h2>
+                                Learn Design Thinking
+                                like a Pro and Build
+                                Projects that People
+                                will care about
+                            </h2>
+                            
+                            <span>Theresa Brazen, Apr 9, 2020</span>
+                        </article>
+                        <article>
+                            <img src={require("./stand-project/journal-intro.png")} alt="journal" />
+                            <a href="#">INSPIRATION</a>
+                            <h2>
+                                Learn Design Thinking
+                                like a Pro and Build
+                                Projects that People
+                                will care about
+                            </h2>
+                            
+                            <span>David Mong, Apr 9, 2020</span>
+                        </article>
+                        <article>
+                            <img src={require("./stand-project/journal-intro.png")} alt="journal" />
+                            <a href="#">INSPIRATION</a>
+                            <h2>
+                                Learn Design Thinking
+                                like a Pro and Build
+                                Projects that People
+                                will care about
+                            </h2>
+                            
+                            <span>David Mong, Apr 9, 2020</span>
+                        </article>
+                        <article>
+                            <img src={require("./stand-project/journal-intro.png")} alt="journal" />
+                            <a href="#">INSPIRATION</a>
+                            <h2>
+                                Learn Design Thinking
+                                like a Pro and Build
+                                Projects that People
+                                will care about
+                            </h2>
+                            
+                            <span>David Mong, Apr 9, 2020</span>
+                        </article>
+                        <article>
+                            <img src={require("./stand-project/journal-intro.png")} alt="journal" />
+                            <a href="#">INSPIRATION</a>
+                            <h2>
+                                Learn Design Thinking
+                                like a Pro and Build
+                                Projects that People
+                                will care about
+                            </h2>
+                            
+                            <span>Theresa Brazen, Apr 9, 2020</span>
+                        </article>
+                    </div>
+                    <div className="load-more">
+                        <button>LOAD MORE</button>
+                    </div>
+                </section>
+                <section className="subscribe-section" id="subscribe-section">
+                    <img src={require("./stand-project/scrollup.png")}/>
+                    <div style={{backgroundColor:"#EAC435"}}>
+                        <h2>Stay Up To Date On What We Are Doing And New Learning Opportunities</h2>
+                        <form>
+                            <input type="email" placeholder="YOUR EMAIL"/>
+                            <button className="subscribe-btn">SUBSCRIBE</button>
+                            <a>View Privacy Policy</a>
+                        </form>
+                    </div>
+                </section>
+            </main>
+        )
+    }
+}
+export default Journal;
