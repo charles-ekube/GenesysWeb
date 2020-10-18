4c4af19cb49ceacc591dee59861885c0cef1161b
Charles Ekube <60458135+charles-ekube@users.noreply.github.com>
2020-10-18 15:18:10 +0100

    final update

--- src/FaqPage.js
+++ src/FaqPage.js
@@ -12,80 +12,80 @@ class Faq extends React.Component {
                 <main>
                 <section className="questions-intro">
                     <h1 id="questions-intro-head">FAQ</h1>
-                    <img src={require("./stand-project/faq.png")} alt="faq"  id="questions-intro-img"/>
+                    <img src={require("./stand-project/faq.png")}  id="questions-intro-img" alt="faq"/>
                 </section>
                 <section className="questions-link">
                     <ul>
                         <li>
                             How many people can attend a course?
-                        <span></span>
+                        <span> <i className="fa fa-angle-down"></i></span>
                         </li>
                         <li>
                             How many people can attend a course?
-                        <span></span>
+                        <span> <i className="fa fa-angle-down"></i></span>
                         </li>
                         <li>
                             How many people can attend a course?
-                        <span></span>
+                        <span><i className="fa fa-angle-down"></i></span>
                         </li>
                         <li>
                             How many people can attend a course?
-                        <span></span>
+                        <span><i className="fa fa-angle-down"></i></span>
                         </li>
                         <li>
                             How many people can attend a course?
-                        <span></span>
+                        <span><i className="fa fa-angle-down"></i></span>
                         </li>
                         <li>
                             How many people can attend a course?
-                        <span></span>
+                        <span><i className="fa fa-angle-down"></i></span>
                         </li>
                         <li>
                             How many people can attend a course?
-                        <span></span>
+                        <span><i className="fa fa-angle-down"></i></span>
                         </li>
                         <li>
                             How many people can attend a course?
-                        <span></span>
+                        <span><i className="fa fa-angle-down"></i></span>
                         </li>
                         <li>
                             How many people can attend a course?
-                        <span></span>
+                        <span><i className="fa fa-angle-down"></i></span>
                         </li>
                         <li>
                             How many people can attend a course?
-                        <span></span>
+                        <span><i className="fa fa-angle-down"></i></span>
                         </li>
                         <li>
                             How many people can attend a course?
-                        <span></span>
+                        <span><i className="fa fa-angle-down"></i></span>
                         </li>
                         <li>
                             How many people can attend a course?
-                        <span></span>
+                        <span><i className="fa fa-angle-down"></i></span>
                         </li>
                         <li>
                             How many people can attend a course?
-                        <span></span>
+                        <span><i className="fa fa-angle-down"></i></span>
                         </li>
                         <li>
                             How many people can attend a course?
-                        <span></span>
+                        <span><i className="fa fa-angle-down"></i></span>
                         </li>
                         <li>
                             How many people can attend a course?
-                        <span></span>
+                        <span><i className="fa fa-angle-down"></i></span>
                         </li>
                     </ul>
                 </section>
                 <section className="subscribe-section" id="subscribe-section">
-                    <img src={require("./stand-project/scrollup.png")} alt="scrollup"/>
+                    <img src={require("./stand-project/scrollup.png")} alt="new"/>
                     <div style={{backgroundColor:"#EAC435"}}>
                         <h2>Stay Up To Date On What We Are Doing And New Learning Opportunities</h2>
                         <form>
                             <input type="email" placeholder="YOUR EMAIL"/>
                             <button className="subscribe-btn">SUBSCRIBE</button>
-                            <a href="#">View Privacy Policy</a>
+                            <p>View Privacy Policy</p>
                         </form>
                     </div>
                 </section>
