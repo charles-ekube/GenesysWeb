fd980b48f81002045cd0d4eeaf8fe81034232673
Charles Ekube <60458135+charles-ekube@users.noreply.github.com>
2020-10-18 04:04:37 +0100

    corrected error warnings

--- src/FaqPage.js
+++ src/FaqPage.js
@@ -12,7 +12,7 @@ class Faq extends React.Component {
                 <main>
                 <section className="questions-intro">
                     <h1 id="questions-intro-head">FAQ</h1>
-                    <img src={require("./stand-project/faq.png")}  id="questions-intro-img"/>
+                    <img src={require("./stand-project/faq.png")} alt="faq"  id="questions-intro-img"/>
                 </section>
                 <section className="questions-link">
                     <ul>
@@ -79,13 +79,13 @@ class Faq extends React.Component {
                     </ul>
                 </section>
                 <section className="subscribe-section" id="subscribe-section">
-                    <img src={require("./stand-project/scrollup.png")}/>
+                    <img src={require("./stand-project/scrollup.png")} alt="scrollup"/>
                     <div style={{backgroundColor:"#EAC435"}}>
                         <h2>Stay Up To Date On What We Are Doing And New Learning Opportunities</h2>
                         <form>
                             <input type="email" placeholder="YOUR EMAIL"/>
                             <button className="subscribe-btn">SUBSCRIBE</button>
-                            <a>View Privacy Policy</a>
+                            <a href="#">View Privacy Policy</a>
                         </form>
                     </div>
                 </section>
