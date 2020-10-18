e6f1f273b4c2b01eb4a177f62ca519d763d3243c
Charles Ekube <60458135+charles-ekube@users.noreply.github.com>
2020-10-18 02:05:09 +0100

    Completed the Learnable Navbar

---
+++ src/LearnableNav.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+class LearnableNav extends React.Component {
+    render() {
+        return (
+            <nav className="learnable-nav">
+                <ul>
+                    <li>
+                        <Link to="/LearnableHomePage" className="link">HOME</Link>
+                    </li>
+                    <li>
+                        <Link to="/SoftwareDevelopersPage" className="link">SOFTWARE DEVELOPERS</Link>
+                    </li>
+                    <li>
+                        <Link to="/ProductDesignPage" className="link">PRODUCT DESINGERS</Link>
+                    </li>
+                    <li>
+                        <Link to="/ApplyPage" className="link">APPLY</Link>
+                    </li>
+                    <li>
+                        <Link to="/JournalPage" className="link">JOURNAL</Link>
+                    </li>
+                    <li>
+                        <Link to="/FaqPage" className="link">FAQ</Link>
+                    </li>
+                </ul>
+                {/* </Router> */}
+            </nav>
+        )
+    }
+}
+
+export default LearnableNav;
